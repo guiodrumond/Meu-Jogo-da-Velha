b6e9b94bb2272720fc9d08b94558734d5831e160
Guilherme Oliveira Drumond <guilherme.odrumond@sp.senac.br>
2022-07-07 12:11:27 -0300

    Últimas implementações para o jogo da velha funcional

--- src/Game.js
+++ src/Game.js
@@ -24,7 +24,39 @@ const getLabel = (value) => {
 }
 
 function getWinner(values) {
-
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+            const sumRow =
+                values[`${r}-${c}`] +
+                values[`${r}-${c+1}`] +
+                values[`${r}-${c+2}`];
+            if (sumRow === 3 || sumRow === -3) {
+                return sumRow;
+            }
+            const sumCol =
+                values[`${r}-${c}`] +
+                values[`${r+1}-${c}`] +
+                values[`${r+2}-${c}`];
+            if (sumCol === 3 || sumCol === -3) {
+                return sumCol;
+            }
+            const sumDiag =
+                values[`${r}-${c}`] +
+                values[`${r+1}-${c+1}`] +
+                values[`${r+2}-${c+2}`];
+            if (sumDiag === 3 || sumDiag === -3) {
+                return sumDiag;
+            }
+            const sumRevDiag =
+                values[`${r}-${c}`] +
+                values[`${r+1}-${c-1}`] +
+                values[`${r+2}-${c-2}`];
+            if (sumRevDiag === 3 || sumRevDiag === -3) {
+                return sumRevDiag;
+            }
+        }
+    }
+    return null;
 }
 
 const Game = () => {
@@ -36,18 +68,30 @@ const Game = () => {
         if (winner || values[key]) {
             return;
         }
-        setValues({
+        const newValues = {
             ...values,
             [key]: player,
-        });
+        };
+        setValues(newValues);
 
         setPlayer(player * -1);
-        const newWinner = getWinner(values);
+        const newWinner = getWinner(newValues);
 
         if (newWinner) {
-            setWinner()
+            setWinner(newWinner > 0 ? 1 : -1);
         }
     }
+
+    function reset() {
+        setWinner(null);
+        setValues(getInitialState);
+        setPlayer(1);
+    };
+
+    const isATie = Object
+        .values(values)
+        .filter(Boolean)
+        .length === 9 && !winner;
 
     return (
 
@@ -61,7 +105,7 @@ const Game = () => {
                         <button
                             key={index}
                             type="button"
-                            onClick={() => 
+                            onClick={() =>
                                 handleClick(key)}
                         >
                             {getLabel(values[key])}
@@ -69,12 +113,14 @@ const Game = () => {
                     )
                 })}
             </div>
-            <div className="Game__menu">
-                <p>O ganhador é: {winner > 0 ? 'O' : 'x'}</p>
 
-                {/* Vídeo em 23:54 */}
-
-            </div>
+            {(winner || isATie) && (
+                <div className="Game__menu">
+                    {winner ?
+                        (<p>O ganhador é: {winner > 0 ? 'O' : 'x'}</p>) :
+                        (<p>Deu velha!</p>)}                    
+                    <button onClick={reset}>Reiniciar</button>
+                </div>)}
         </div>
 
     )
